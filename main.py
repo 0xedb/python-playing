037ba69718a860f0f9c612941ab9e49f48689c5e
bruno edoh <edohbruno@gmail.com>
2019-12-06 22:47:12 +0000

    classes play

--- main.py
+++ main.py
@@ -1,7 +1,31 @@
 def func(first, *next, **rest):
-  type(first)
-  type(next)
-  type(rest)
+    print((first))
+    print((next))
+    print((rest))
 
 
-func('a', 332, 32, a = 3932, b = 3939)
+func('a', 332, 32, a=3932, b=3939)
+
+
+def swapper(x, y):
+    return lambda z: y, x
+
+
+another = swapper(10, 11)
+print(another)
+
+name = "Bruno"
+id = 3937
+print(f"{name} ------ {id}")
+
+
+class Human:
+    def __init__(self, name):
+        self.name = name
+
+    @property
+    def email(self):
+        return self.email
+
+
+bruno = Human('Kofi')
